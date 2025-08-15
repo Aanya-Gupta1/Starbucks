2a96c1bc104518bffea139d94a328eb823b8b392
Aanya-Gupta1 <167212727+Aanya-Gupta1@users.noreply.github.com>
2025-08-15 23:29:55 +0530

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>    
+<?php
+// Server-side logic to fetch dynamic content
+$dynamicContent = "This is dynamic content generated on the server side.";
+
+echo "<p>$dynamicContent</p>";
+?>
+</body>
+</html>
